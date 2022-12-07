17519e33fe28f10bdb708eed5f2e4d0ce9d30b43
[ansquf0715] <ansquf0715@naver.com>
2022-12-07 23:19:29 +0900

    camera move..

--- Assets/Byeol/Scripts/CameraMove.cs
+++ Assets/Byeol/Scripts/CameraMove.cs
@@ -6,11 +6,12 @@ public class CameraMove : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
+    //public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,5 +19,13 @@ public class CameraMove : MonoBehaviour
     {
         transform.position = target.position + offset;
     }
+
+    //private void LateUpdate()
+    //{
+    //    Vector3 direction = (player.transform.position - transform.position).normalized;
+    //    RaycastHit[] hits = Physics.RaycastAll(transform.position, direction, Mathf.Infinity,
+    //        1 << LayerMask.NameToLayer("EnvironmentObject"));
+
+    //}
 
 }
