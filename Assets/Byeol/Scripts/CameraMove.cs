8c3e514a5e8c4475909b065df8ed877470c209af
[ansquf0715] <ansquf0715@naver.com>
2023-04-21 23:39:47 +0900

    플레이어 바다쪽으로 이동, 배랑 공주추가

--- Assets/Byeol/Scripts/CameraMove.cs
+++ Assets/Byeol/Scripts/CameraMove.cs
@@ -25,23 +25,43 @@ public class CameraMove : MonoBehaviour
     void Update()
     {
         transform.position = target.position + offset;
+
+        foreach (KeyValuePair<GameObject, Color> pair in originalTreeColors)
+        {
+            Renderer renderer = pair.Key.GetComponent<MeshRenderer>();
+
+            if (renderer != null)
+            {
+                Material material = renderer.material;
+                Color color = material.color;
+                color.a = 1f; // Set alpha to 1
+                material.color = color;
+            }
+        }
         changeTreeBlocksScreen();
     }
 
     void changeTreeBlocksScreen()
     {
-        RaycastHit[] hits = Physics.RaycastAll(Camera.main.transform.position,
-            (Player.playerPos - Camera.main.transform.position).normalized,
-            Vector3.Distance(Camera.main.transform.position, Player.playerPos));
+        //originalTreeColors.Clear();
 
-        // Create a copy of originalTreeColors for oldTreeColors
-        if (originalTreeColors.Count > 0 && oldTreeColors.Count == 0)
+        // Calculate the difference between originalTreeColors and oldTreeColors
+        Dictionary<GameObject, Color> missingColors = new Dictionary<GameObject, Color>();
+        foreach (KeyValuePair<GameObject, Color> pair in oldTreeColors)
         {
-            foreach (KeyValuePair<GameObject, Color> pair in originalTreeColors)
+            if (!originalTreeColors.ContainsKey(pair.Key))
             {
-                oldTreeColors.Add(pair.Key, pair.Value);
+                missingColors.Add(pair.Key, pair.Value);
             }
         }
+
+        Vector3 direction = (Player.playerPos - Camera.main.transform.position).normalized;
+        float distance = Vector3.Distance(Camera.main.transform.position, Player.playerPos);
+
+        RaycastHit[] hits = Physics.RaycastAll(Camera.main.transform.position, direction, distance);
+
+        //RaycastHit[] hits = Physics.RaycastAll(Camera.main.transform.position,
+        //    Camera.main.transform.forward,  Mathf.Infinity);
 
         // Update originalTreeColors
         foreach (RaycastHit hit in hits)
@@ -70,19 +90,30 @@ public class CameraMove : MonoBehaviour
                     material.renderQueue = 3000;
 
                     Color color = material.color;
-                    color.a = 0.5f;
+                    color.a = 0.1f;
                     material.color = color;
 
                     // Remove the game object from oldTreeColors if it exists
                     if (oldTreeColors.ContainsKey(hit.collider.gameObject))
                     {
-                        Debug.Log("여기도 걸리니");
                         oldTreeColors.Remove(hit.collider.gameObject);
                     }
                 }
             }
         }
 
+        foreach (KeyValuePair<GameObject, Color> pair in missingColors)
+        {
+            Renderer renderer = pair.Key.GetComponent<MeshRenderer>();
+
+            if (renderer != null)
+            {
+                Material material = renderer.material;
+                Color color = material.color;
+                color.a = 1f; // Set alpha to 1
+                material.color = color;
+            }
+        }
 
         // Restore oldTreeColors that are not in originalTreeColors
         foreach (KeyValuePair<GameObject, Color> pair in oldTreeColors)
@@ -92,72 +123,20 @@ public class CameraMove : MonoBehaviour
             if (renderer != null)
             {
                 Material material = renderer.material;
-
                 Color color = material.color;
                 color.a = 1f; // Set alpha to 1
                 material.color = color;
             }
         }
+
+        // Create a copy of originalTreeColors for oldTreeColors
+        if (originalTreeColors.Count > 0 && oldTreeColors.Count == 0)
+        {
+            foreach (KeyValuePair<GameObject, Color> pair in originalTreeColors)
+            {
+                oldTreeColors.Add(pair.Key, pair.Value);
+            }
+        }
+
     }
-
-    //void changeTreeBlocksScreen()
-    //{
-    //    if (Physics.Raycast(Camera.main.transform.position,
-    //        (Player.playerPos - Camera.main.transform.position).normalized,
-    //        out RaycastHit hit, Vector3.Distance(Camera.main.transform.position, Player.playerPos)))
-    //    {
-    //        if (hit.collider.CompareTag("Tree"))
-    //        {
-    //            Renderer renderer = hit.collider.GetComponent<MeshRenderer>();
-
-    //            if (renderer != null)
-    //            {
-    //                Debug.Log("if3");
-    //                Material material = renderer.material;
-
-    //                material.SetFloat("_Mode", 3); //set rendering mode to "Transparent"
-    //                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-    //                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-    //                material.SetInt("_ZWrite", 0);
-    //                material.DisableKeyword("_ALPHATEST_ON");
-    //                material.EnableKeyword("_ALPHABLEND_ON");
-    //                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-    //                material.renderQueue = 3000;
-
-    //                if (!originalTreeColors.ContainsKey(hit.collider.gameObject))
-    //                {
-    //                    originalTreeColors.Add(hit.collider.gameObject, material.color);
-    //                }
-
-    //                //originalTreeColors[hit.collider.gameObject] = material.color;
-    //                //Debug.Log("original tree colors " + originalTreeColors);
-
-    //                Color color = material.color;
-    //                color.a = 0.5f;
-    //                material.color = color;
-    //            }
-    //        }
-    //        else
-    //        {
-    //            Debug.Log("original tree color count" + originalTreeColors.Count);
-    //            if (originalTreeColors.Count > 0)
-    //            {
-    //                foreach (KeyValuePair<GameObject, Color> entry in originalTreeColors)
-    //                {
-    //                    Renderer renderer = entry.Key.GetComponent<MeshRenderer>();
-    //                    Material material = renderer.material;
-
-    //                    if (material != null && entry.Value != null)
-    //                    {
-    //                        material.color = entry.Value;
-    //                    }
-    //                }
-
-    //                originalTreeColors.Clear();
-    //            }
-    //        }
-    //    }
-    //}
-
-
 }
---
+++ Assets/Byeol/Scripts/Princess.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Princess : MonoBehaviour
+{
+    List<Dictionary<string, object>> princessTalking;
+    int talkingNum = 0;
+
+    RaycastHit hit;
+
+    public GameObject npcUI;
+    public GameObject backGroundTalking;
+    public GameObject npcPortrait;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
