5519f1069f8d3716af2dd66d24864bd9469d661d
[ansquf0715] <ansquf0715@naver.com>
2023-04-30 23:52:59 +0900

    보스 네비메시 수정, player 만나는 지점 설정, 애니메이터 만들기

--- Assets/Byeol/Scripts/Boss.cs
+++ Assets/Byeol/Scripts/Boss.cs
@@ -14,6 +14,13 @@ public class Boss : MonoBehaviour
     BoxCollider fightingZoneCollider;
     Bounds fightingZoneBound;
 
+    public Collider[] cols = new Collider[3];
+
+    Collider boxCol;
+    Animator anim;
+
+    GameObject moveParticle;
+
     public bool playerIsInFightingZone = false;
 
     public float obstacleDetectionRadius = 1.0f;
@@ -28,13 +35,13 @@ public class Boss : MonoBehaviour
         fightingZoneCollider = fightingZone.GetComponent<BoxCollider>();
         fightingZoneBound = fightingZoneCollider.bounds;
 
-        Vector3 tempTarget = new Vector3(-4f, 0, 38f);
-        agent.SetDestination(tempTarget);
+        GameObject bossP = GameObject.Find("BossP");
+        cols = bossP.GetComponentsInChildren<Collider>();
 
-        Debug.Log("fightingZoneBound" + fightingZoneBound.min.z);
-        Debug.Log("fightingZoneBound" + fightingZoneBound.max.z);
+        boxCol = GetComponent<BoxCollider>();
+        anim = GetComponent<Animator>();
 
-        //rigid.velocity = new Vector3(0, -1, 0);
+        agent.SetDestination(setRandomPos());
     }
 
     // Update is called once per frame
@@ -51,116 +58,78 @@ public class Boss : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+        {
+            //Debug.Log(" meet player");
+            agent.isStopped = true;
+            anim.SetBool("meetPlayer", true);
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.name == "FightingZonePlane"
             && other.bounds.Contains(Player.playerPos))
         {
-            //Debug.Log("Player is inside the plane");
             playerIsInFightingZone = true;
         }
     }
 
-    //void findToGo()
-    //{
-    //    if (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
-    //        return;
-
-    //    NavMeshPath path = new NavMeshPath();
-
-    //    if (agent.CalculatePath(agent.destination, path) && path.status == NavMeshPathStatus.PathComplete)
-    //    {
-    //        // 길이 유효한 경우
-    //        agent.SetDestination(agent.destination);
-    //    }
-    //    else
-    //    {
-    //        // 길이 유효하지 않은 경우
-    //        Vector3 newDestination = GetRandomPointInBounds(fightingZoneBound);
-
-    //        agent.SetDestination(newDestination);
-    //    }
-    //}
-
-    void OnCollisionEnter(Collision collision)
+    Vector3 setRandomPos() //col 3개 중 한 군데 랜덤으로 정해서 그 안에서 랜덤 포지션
     {
-        // 충돌한 객체가 NavMeshObstacle인 경우
-        if (collision.gameObject.GetComponent<NavMeshObstacle>() != null)
+        int randomCol = Random.Range(0, 3);
+        Bounds bounds;
+        Vector3 randomPos;
+
+        switch(randomCol)
         {
-            Vector3 newDestination = GetRandomPointInBounds(fightingZoneBound);
-
-            agent.SetDestination(newDestination);
-        }
-    }
-
-    Vector3 GetRandomPointInBounds(Bounds bounds)
-    {
-        Vector3 randomPos = new Vector3(
-            Random.Range(bounds.min.x, bounds.max.x),
-            0,
-            Random.Range(bounds.min.z, bounds.max.z));
-
-        NavMeshHit hit;
-
-        if (NavMesh.SamplePosition(randomPos, out hit, obstacleDetectionRadius, NavMesh.AllAreas))
-        {
-            return hit.position;
-        }
-        else
-        {
-            return GetRandomPointInBounds(bounds);
+            case 0: //0번 col
+                bounds = cols[0].bounds;
+                randomPos = new Vector3(
+                    Random.Range(bounds.min.x, bounds.max.x),
+                    0,
+                    Random.Range(bounds.min.z, bounds.max.z));
+                Debug.Log("random Pos col 0" + randomPos);
+                return randomPos;
+            case 1: //1번 col
+                bounds = cols[1].bounds;
+                randomPos = new Vector3(
+                    Random.Range(bounds.min.x, bounds.max.x),
+                    0,
+                    Random.Range(bounds.min.z, bounds.max.z));
+                Debug.Log("random Pos col 1" + randomPos);
+                return randomPos;
+            case 2: //2번 col
+                bounds = cols[2].bounds;
+                randomPos = new Vector3(
+                    Random.Range(bounds.min.x, bounds.max.x),
+                    0,
+                    Random.Range(bounds.min.z, bounds.max.z));
+                Debug.Log("random Pos col 2" + randomPos);
+                return randomPos;
+            default:
+                return Vector3.zero;
         }
     }
 
     void findToGo()
     {
-        if (agent.pathPending || agent.remainingDistance > agent.stoppingDistance)
-            return;
-
-        NavMeshPath path = new NavMeshPath();
-
-        if (agent.CalculatePath(agent.destination, path) && path.status == NavMeshPathStatus.PathComplete)
+        Debug.Log("find to go");
+        if(playerIsInFightingZone) //player가 fightingZone안에 있으면
         {
-            Vector3 newDestination = new Vector3(
-                Random.Range(fightingZoneBound.min.x, fightingZoneBound.max.x),
-                0,
-                Random.Range(fightingZoneBound.min.z, fightingZoneBound.max.z));
-
-            agent.SetDestination(newDestination);
+            Debug.Log("player is in fighting zone");
+            agent.SetDestination(Player.playerPos);
         }
         else
         {
-            // 길이 유효하지 않은 경우
-            Vector3 newDestination = new Vector3(
-                Random.Range(fightingZoneBound.min.x, fightingZoneBound.max.x),
-                0,
-                Random.Range(fightingZoneBound.min.z, fightingZoneBound.max.z));
-
-            agent.SetDestination(newDestination);
-            Debug.Log("else문에 걸려?");
-
+            if (agent.remainingDistance < 1f && !agent.pathPending)
+            {
+                Debug.Log("player is not in fighting zone");
+                agent.SetDestination(setRandomPos());
+            }
         }
     }
 
-    //void findToGo()
-    //{
-    //    if (playerIsInFightingZone) //player가 fightingZone안에 있을 때
-    //    {
-    //        agent.SetDestination(Player.playerPos);
-    //    }
-    //    else
-    //    {
-    //        if (agent.pathStatus == NavMeshPathStatus.PathComplete
-    //            && agent.remainingDistance <= agent.stoppingDistance)
-    //        {
-    //            //목적지에 도달한 경우
-    //            Vector3 randomPos = new Vector3(
-    //                Random.Range(fightingZoneBound.min.x, fightingZoneBound.max.x),
-    //                0,
-    //                Random.Range(fightingZoneBound.min.x, fightingZoneBound.max.x));
-    //            Debug.Log("random Pos" + randomPos);
-    //            agent.SetDestination(randomPos);
-    //        }
-    //    }
-    //}
 }
